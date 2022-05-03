2cfbf702520c980ef00fd3c8c223b559073cd11a
joe <qj5657@gmali.com>
2022-05-04 00:39:18 +0800

    three-news-op修改顶级await方式

--- 003.threejs模拟新闻联播片头/three-news-op/vite.config.js
+++ 003.threejs模拟新闻联播片头/three-news-op/vite.config.js
@@ -1,5 +1,13 @@
-export default {
-  build: {
-    target: "esnext",
-  },
-};
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({
+  plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
+});
